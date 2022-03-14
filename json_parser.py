0ae77f11b3ad4c9bca725cef533e6c952e21b433
Clement Fung <clementf@andrew.cmu.edu>
2022-03-14 15:35:02 -0400

    Add full parser script

--- json_parser.py
+++ json_parser.py
@@ -3,8 +3,11 @@ import pdb
 
 def parse_annotations(filename, output_tag, filtering=False):
 
-    total_records = []
-    total_filenames = []
+    total_records_fr = []
+    total_filenames_fr = []
+
+    total_records_us = []
+    total_filenames_us = []
 
     with open(filename, "r") as f:
         
@@ -23,29 +26,47 @@ def parse_annotations(filename, output_tag, filtering=False):
 
             category_id = x['annotations'][i]['category_id']
 
-            # Try using only the first 10 categories
+            # Try using only the first 20 categories
             if filtering and category_id > 20:
                 continue
-
+                
             new_record = dict()
             for key in x['images'][i].keys():
 
                 new_record[key] = x['images'][i][key]
                 new_record['category'] = category_id
 
-            total_filenames.append(x['images'][i]['file_name'])
-            total_records.append(new_record)
+            country = x['images'][i]["country"]
 
-    print(f'Final output length: {len(total_records)}')
+            if country == "fr":
+                total_filenames_fr.append(x['images'][i]['file_name'])
+                total_records_fr.append(new_record)
+            elif country == "us":
+                total_filenames_us.append(x['images'][i]['file_name'])
+                total_records_us.append(new_record)
+            else:
+                print(f'Unknown country: {country}')
+
+    print(f'Final output length (fr): {len(total_records_fr)}')
+    print(f'Final output length (us): {len(total_records_us)}')
 
     if filtering:
         output_tag = f'{output_tag}_filtered'
 
-    with open(f'{output_tag}_parsed.json', "w") as output:
-        json.dump(total_records, output)
+    # Save france files
+    with open(f'{output_tag}_fr_parsed.json', "w") as output:
+        json.dump(total_records_fr, output)
 
-    with open(f'{output_tag}_files.txt', "w") as output:
-        for file in sorted(total_filenames):
+    with open(f'{output_tag}_fr_files.txt', "w") as output:
+        for file in sorted(total_filenames_fr):
+            output.write(f'{file}\n')
+
+    # Save US files
+    with open(f'{output_tag}_us_parsed.json', "w") as output:
+        json.dump(total_records_us, output)
+
+    with open(f'{output_tag}_us_files.txt', "w") as output:
+        for file in sorted(total_filenames_us):
             output.write(f'{file}\n')
 
 if __name__ == '__main__':
@@ -55,4 +76,4 @@ if __name__ == '__main__':
     # Remove the '.json from the filename'
     output_tag = filename[:-5]
 
-    parse_annotations(filename, output_tag, filtering=False)
+    parse_annotations(filename, output_tag, filtering=True)
---
+++ patches/dl_and_filter_npy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
+
+### BE CAREFUL. This script downloads, reads and deletes files in a loop.
+
+## A fairly messy script that will:
+# (i) download all 20 tar.gz files, one at a time
+# (ii) untar them 
+# (iii) search the new directory for any file not listed in the filtered txt file
+# (iv) and deletes them if not found
+
+#tar_ids=('01' '02' '03' '04' '05' '06' '07' '08' '09' '10' '11' '12' '13' '14' '15' '16' '17' '18' '19' '20')
+tar_ids=('03' '06' '07' '08' '09')
+
+for tid in ${tar_ids[@]}; do
+
+  echo "downloading $tid"  
+  ./azcopy cp "https://lilablobssc.blob.core.windows.net/geolifeclef-2020/patches_fr_$tid.tar.gz" "patches_fr_$tid.tar.gz"
+  tar xvf "patches_fr_$tid.tar.gz"
+  rm "patches_fr_$tid.tar.gz"
+
+  echo "cleaning $tid"  
+  search_dir="./patches_fr_$tid"
+  cd "$search_dir"
+
+  # TODO: parse and keep the altitude files too (probably easiest to just include them in the txt)
+  for entry in */*/*.npy; do
+    if grep -q "$entry" ../annotations_train_filtered_fr_files.txt; then
+      echo "keep $entry" 
+    else
+      echo "delete $entry"
+      rm $entry
+    fi
+  done
+
+  cd ..
+
+done
