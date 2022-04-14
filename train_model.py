6435077460297576fd19dd6dbcf75b7953d933d3
Clement Fung <clement.y.fung@gmail.com>
2022-04-14 17:14:18 -0400

    Add argparse, prep for grid search

--- train_model.py
+++ train_model.py
@@ -1,3 +1,4 @@
+import argparse
 import numpy as np
 import tensorflow as tf
 import pdb
@@ -9,27 +10,22 @@ from tensorflow.keras import regularizers
 from tensorflow.keras import optimizers
 from keras.utils import np_utils
 
-def create_model(n_classes=10):
+def create_model(n_classes=10, n_units=16, n_layers=2, kernel=5, reg_weight=0.1):
 	""" Creates Keras CNN model.   """
 
 	# retrieve params
-	units = 16
-	layers = 2
 	activation = 'relu'
-	kernel_size = 5
-	reg_weight = 0.1
-
 	verbose = True
 
 	input_shape = (256, 256, 3)
 
 	input_layer = Input(shape=input_shape)
-	cnn_layer = Conv2D(filters=units, kernel_size=kernel_size, activation=activation, 
+	cnn_layer = Conv2D(filters=n_units, kernel_size=kernel, activation=activation, 
 		kernel_regularizer=regularizers.l2(reg_weight))(input_layer)
 	cnn_layer = BatchNormalization()(cnn_layer)
 
-	for _ in range(layers - 1):
-		cnn_layer = Conv2D(filters=units, kernel_size=kernel_size, activation=activation, 
+	for _ in range(n_layers - 1):
+		cnn_layer = Conv2D(filters=n_units, kernel_size=kernel, activation=activation, 
 			kernel_regularizer=regularizers.l2(reg_weight))(cnn_layer)
 		cnn_layer = BatchNormalization()(cnn_layer)
 
@@ -61,18 +57,54 @@ def data_generator(X, y, bs):
 		y_batch = y[i:i+bs]
 		yield (X_batch, y_batch)
 
+def get_argparser():
+	
+	parser = argparse.ArgumentParser()
+
+	### CNNs
+	parser.add_argument("--cnn_units", 
+		default=16,
+		type=int,
+		help="Number of units in hidden layers of the CNN")
+	parser.add_argument("--cnn_layers", 
+		default=2,
+		type=int,
+		help="Number of layers in the CNN")
+	parser.add_argument("--cnn_kernel", 
+		default=5,
+		type=int,
+		help="Kernel Size of the CNN")
+	parser.add_argument("--cnn_reg", 
+		default=0.1,
+		type=float,
+		help="Regularization weight of the CNN")
+
+	return parser
+
 if __name__ == '__main__':
 	
-	### A minimal working example of a trained CNN on some fake data.
-
 	n_classes = 20
-
 	n_samples = 14363
 	split_idx = 11000
 	local = False
 
-	model = create_model(n_classes=n_classes)
+	parser = get_argparser()
+	args = parser.parse_args()
+
+	########################
+	# Define CNN model
+	########################
+	units = args.cnn_units
+	layers = args.cnn_layers
+	kernel = args.cnn_kernel
+	regularizer = args.cnn_reg
+
+	model_name = f'CNN-layers{layers}-units{units}-kernel{kernel}-reg{regularizer}'
+	model = create_model(n_classes=n_classes, n_units=units, n_layers=layers, kernel=kernel, reg_weight=regularizer)
 	
+	########################
+	# Load and process data
+	########################
 	Xrgb = np.load('Xrgb_filtered.npy')
 	yrgb = np.load('yrgb_filtered.npy')
 
@@ -96,33 +128,34 @@ if __name__ == '__main__':
 		Xtest = Xrgb[split_idx:]
 		ytest = yrgb_cat[split_idx:]
 
-		batch_size = 32
-		epoch_steps = len(Xtrain) // batch_size
-		val_steps = len(Xtest) // batch_size
+	########################
+	# Train the model
+	########################
+	batch_size = 32
+	epoch_steps = len(Xtrain) // batch_size
+	val_steps = len(Xtest) // batch_size
 
 	train_history = model.fit_generator(
 		data_generator(Xtrain, ytrain, batch_size), 
 		steps_per_epoch=epoch_steps,
 				validation_steps=val_steps,
 		validation_data=data_generator(Xtest, ytest, batch_size), 
-		epochs=20)
+		epochs=50)
 
-	# train_history = model.fit(Xtrain, ytrain, validation_data=(Xtest, ytest), epochs=5)
+	########################
+	# Save and evaluate model
+	########################
+	model.save(model_name+'.h5')
+	print(f'Keras model saved to {model_name+".h5"}')
 
-	loss_obj = train_history.history['loss']
-	np.save(f'history.npy', loss_obj)
+	loss_obj = np.vstack([train_history.history['loss'], train_history.history['val_loss']])
+	np.savetxt(f'train-history-{model_name}.csv', loss_obj, delimiter=',', fmt='%.5f')
 
 	ypred = model.predict(Xtest)
 	test_accuracy = np.mean(np.argmax(ypred, axis=1) == np.argmax(ytest, axis=1))
 	print(f"test accuracy is {test_accuracy}")
 
-	pdb.set_trace()
-
 	# Sometimes train accuracy crashes OOM. Moved it to the end for now
 	ytrain_pred = model.predict(Xtrain)
 	train_accuracy = np.mean(np.argmax(ytrain_pred, axis=1) == np.argmax(ytrain, axis=1))
 	print(f"train accuracy is {train_accuracy}")
-
-	# Add breakpoint in case some checks are needed
-	pdb.set_trace()
-
