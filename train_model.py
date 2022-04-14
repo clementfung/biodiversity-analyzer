966d46fae73708e47a067752ab3250cc4f7edc70
Clement Fung <clement.y.fung@gmail.com>
2022-04-14 16:34:54 -0400

    Support batch based training and eval

--- train_model.py
+++ train_model.py
@@ -17,25 +17,27 @@ def create_model(n_classes=10):
 	layers = 2
 	activation = 'relu'
 	kernel_size = 5
+	reg_weight = 0.1
+
 	verbose = True
 
 	input_shape = (256, 256, 3)
 
 	input_layer = Input(shape=input_shape)
 	cnn_layer1 = Conv2D(filters=units, kernel_size=kernel_size, activation=activation, 
-		kernel_regularizer=regularizers.l2(1))(input_layer)
+		kernel_regularizer=regularizers.l2(reg_weight))(input_layer)
 	cnn_layer1 = BatchNormalization()(cnn_layer1)
 	
 	cnn_layer2 = Conv2D(filters=units, kernel_size=kernel_size, activation=activation, 
-		kernel_regularizer=regularizers.l2(1))(cnn_layer1)
+		kernel_regularizer=regularizers.l2(reg_weight))(cnn_layer1)
 	cnn_layer2 = BatchNormalization()(cnn_layer2)
 
 	cnn_layer3 = Conv2D(filters=units, kernel_size=kernel_size, activation=activation, 
-		kernel_regularizer=regularizers.l2(1))(cnn_layer2)
+		kernel_regularizer=regularizers.l2(reg_weight))(cnn_layer2)
 	cnn_layer3 = BatchNormalization()(cnn_layer3)
 
 	flatten = Flatten()(cnn_layer3)
-	dense_out = Dense(n_classes, activation='softmax', kernel_regularizer=regularizers.l2(1))(flatten)
+	dense_out = Dense(n_classes, activation='softmax', kernel_regularizer=regularizers.l2(reg_weight))(flatten)
 	
 	# Define the total model
 	model = Model(input_layer, dense_out)
@@ -47,6 +49,21 @@ def create_model(n_classes=10):
 	# compile and return model
 	return model
 
+# Generic data generator object for feeding data to fit_generator
+def data_generator(X, y, bs):
+	
+	i = 0
+	while True:
+		i += bs
+
+		# Restart from beginning
+		if i + bs > len(X):
+			i = 0 
+
+		X_batch = X[i:i+bs]
+		y_batch = y[i:i+bs]
+		yield (X_batch, y_batch)
+
 if __name__ == '__main__':
 	
 	### A minimal working example of a trained CNN on some fake data.
@@ -55,7 +72,7 @@ if __name__ == '__main__':
 
 	n_samples = 14363
 	split_idx = 11000
-	local = True
+	local = False
 
 	model = create_model(n_classes=n_classes)
 	
@@ -82,7 +99,19 @@ if __name__ == '__main__':
 		Xtest = Xrgb[split_idx:]
 		ytest = yrgb_cat[split_idx:]
 
-	train_history = model.fit(Xtrain, ytrain, validation_data=(Xtest, ytest), epochs=5)
+		batch_size = 32
+		epoch_steps = len(Xtrain) // batch_size
+		val_steps = len(Xtest) // batch_size
+
+	train_history = model.fit_generator(
+		data_generator(Xtrain, ytrain, batch_size), 
+		steps_per_epoch=epoch_steps,
+				validation_steps=val_steps,
+		validation_data=data_generator(Xtest, ytest, batch_size), 
+		epochs=20)
+
+	# train_history = model.fit(Xtrain, ytrain, validation_data=(Xtest, ytest), epochs=5)
+
 	loss_obj = train_history.history['loss']
 	np.save(f'history.npy', loss_obj)
 
