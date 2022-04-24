72df3b96ab618b87ca5397e68209178f411d0a64
yjiang2cmu <yjiang2@andrew.cmu.edu>
2022-04-23 23:02:14 -0400

    fixed constraints issues

--- linear_programming.py
+++ linear_programming.py
@@ -36,28 +36,31 @@ def construct_B(num_images, num_categories, matrix):
 def solve_lp(num_images, num_categories, budget, num_of_each_species, matrix):
     alloc_all = cp.Variable((1, num_images * num_categories), integer = True)
     alloc_images = cp.Variable((1, num_images), integer = True)
+    alloc_categories = cp.Variable((1, num_categories), integer = True)
 
     # constraints
     constraints = []
     c_image = np.ones(num_images)
     constraints.append(alloc_images @ c_image.T <= budget)
 
-    # budget_images = np.ones(num_images) * num_categories
     A = construct_A(num_images, num_categories)
     constraints.append(alloc_all @ A <= alloc_images * num_categories)
 
     # c_species = [num_of_each_species] @ alloc_images
     B = construct_B(num_images, num_categories, matrix)
-    # constraints.append(alloc_all @ B >= alloc_images * num_of_each_species)
+    constraints.append(alloc_all @ B >= alloc_categories * num_of_each_species)
 
     constraints.append(alloc_all >= 0)
     constraints.append(alloc_all <= 1)
     constraints.append(alloc_images >= 0)
     constraints.append(alloc_images <= 1)
+    constraints.append(alloc_categories >= 0)
+    constraints.append(alloc_categories <= 1)
 
     # objective
     c_categories = np.ones(num_categories)
-    objective = cp.Maximize(cp.sum(alloc_all @ B))
+    # objective = cp.Maximize(cp.sum(alloc_all @ B))
+    objective = cp.Maximize(cp.sum(alloc_categories))
 
     # budget constraint
     problem = cp.Problem(objective, constraints)
@@ -73,7 +76,12 @@ if __name__ == '__main__':
     budget = 5
     num_of_each_species = 2
     matrix = np.zeros((num_images, num_categories))
-    for i in range(0, 5):
+    for i in range(0, 2):
+        matrix[i][0] = 0.25
+        matrix[i][1] = 0.25
+        matrix[i][2] = 0.25
+        matrix[i][3] = 0.25
+    for i in range(2, 5):
         matrix[i][0] = 0.5
         matrix[i][1] = 0.5
     for i in range(5, 10):
